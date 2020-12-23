89de955f8ee8a7cc5858d5324944e1d4cf02aac8
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2020-12-23 12:52:25 -0700

    adds lifecycle hooks for demo purposes

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -11,15 +11,65 @@ export class StockPrice {
   @State() fetchedPrice;
   @State() stockUserInput;
   @State() errorMsg;
+  initialStockSymbol: string;
 
-  @Prop() stockSymbol:string;
+  @Prop() stockSymbol: string;
 
-onFetchStockPrice(event: Event) {
-  event.preventDefault();
-  // const valuestockSynbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
-  const valuestockSynbol = this.stockInput.value;
-  this.fetchStockPrice(valuestockSynbol);
-}
+  onFetchStockPrice(event: Event) {
+    event.preventDefault();
+    // const valuestockSynbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const valuestockSynbol = this.stockInput.value;
+    this.fetchStockPrice(valuestockSynbol);
+  }
+
+  onUserInput(event: Event) {
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+  }
+
+  componentWillLoad() {
+    console.log('component will mount fired')
+    console.log(this.stockSymbol)
+  }
+
+  componentDidLoad() {
+    console.log(this.stockSymbol)
+    if (this.stockSymbol) {
+      this.initialStockSymbol = this.stockSymbol;
+      this.stockUserInput = this.stockSymbol;
+      this.fetchStockPrice(this.stockSymbol);
+    }
+  }
+
+  componentWillUpdate() {
+    console.log('component will update fired')
+    console.log(this.stockSymbol)
+  }
+
+  componentDidUpdate() {
+    console.log('component did update fired')
+    if (this.stockSymbol !== this.initialStockSymbol) {
+      this.initialStockSymbol = this.stockSymbol
+      this.fetchStockPrice(this.stockSymbol);
+    }
+  }
+
+  render() {
+    let data = 'Please enter a symbol';
+    if (this.errorMsg) data = <p>{this.errorMsg}</p>;
+    if (this.fetchedPrice) data = <p>Price: ${this.fetchedPrice}</p>;
+    return [
+      <form onSubmit={this.onFetchStockPrice.bind(this)}>
+        <input type="text" id="stock-symbol" ref={el => this.stockInput = el} value={this.stockUserInput}
+               onInput={this.onUserInput.bind(this)}/>
+        <button type="submit">Fetch</button>
+      </form>,
+      <div>
+        {data}
+      </div>
+    ];
+
+
+  }
 
   private fetchStockPrice(valuestockSynbol: string) {
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${valuestockSynbol}&apikey=2VO8JK6F0U69S6OJ`)
@@ -36,155 +86,5 @@ onFetchStockPrice(event: Event) {
       .catch(err => {
         this.errorMsg = err.message
       });
-  }
-
-  onUserInput(event: Event) {
-  this.stockUserInput = (event.target as HTMLInputElement).value;
-
-}
-
-componentDidLoad() {
-  console.log(this.stockSymbol)
-  if (this.stockSymbol) {
-    this.fetchStockPrice(this.stockSymbol);
-  }
-}
-
-  render () {
-    let data = 'Please enter a symbol';
-    if (this.errorMsg) data = <p>{this.errorMsg}</p>;
-    if (this.fetchedPrice) data = <p>Price: ${this.fetchedPrice}</p>;
-    return [
-      <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input type="text" id="stock-symbol" ref={el => this.stockInput = el} value={this.stockUserInput} onInput={this.onUserInput.bind(this)}/>
-        <button type="submit">Fetch</button>
-      </form>,
-      <div>
-        {data}
-      </div>
-    ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   }
 }
