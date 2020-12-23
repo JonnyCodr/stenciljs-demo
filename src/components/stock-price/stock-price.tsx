7e96406205d196ad9bea60d3390780626bed3392
michael.menard@dhigroupinc.com <michael.menard@dhigroupinc.com>
2020-12-22 19:22:15 -0700

    adds error handling to the fetch result

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -9,27 +9,45 @@ export class StockPrice {
   stockInput: HTMLInputElement;
   // @Element() el: HTMLElement;
   @State() fetchedPrice;
+  @State() stockUserInput;
+  @State() errorMsg;
 
 onFetchStockPrice(event: Event) {
   event.preventDefault();
   // const valuestockSynbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
   const valuestockSynbol = this.stockInput.value;
-  fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTES&symbol=MSFT&apikey=demo`)
+  fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${valuestockSynbol}&apikey=2VO8JK6F0U69S6OJ`)
     .then(res => {
       return res.json();
     })
-    .then(parsedRes => this.fetchedPrice = +parsedRes['Global Quote']['05 price'])
-    .catch(err => console.log(err));
+    .then(parsedRes => {
+      console.log(JSON.stringify(parsedRes))
+      if (!parsedRes['Global Quote']['05. price']) {
+        throw new Error('invalid symbol')
+      }
+      this.fetchedPrice = +parsedRes['Global Quote']['05. price']
+    })
+    .catch(err => {
+      this.errorMsg = err.message
+    });
+}
+
+onUserInput(event: Event) {
+  this.stockUserInput = (event.target as HTMLInputElement).value;
+
 }
 
   render () {
+    let data = 'Please enter a symbol';
+    if (this.errorMsg) data = <p>{this.errorMsg}</p>;
+    if (this.fetchedPrice) data = <p>Price: ${this.fetchedPrice}</p>;
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input type="text" id="stock-symbol" ref={el => this.stockInput = el}/>
+        <input type="text" id="stock-symbol" ref={el => this.stockInput = el} value={this.stockUserInput} onInput={this.onUserInput.bind(this)}/>
         <button type="submit">Fetch</button>
       </form>,
       <div>
-          <p>Price: ${this.fetchedPrice}</p>
+        {data}
       </div>
     ];
 
